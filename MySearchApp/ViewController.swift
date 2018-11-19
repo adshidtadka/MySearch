6e93809f8affad9b08a62ed0e0ce7ffe4a6b7488
gky360 <gky360@gmail.com>
2018-11-20 02:12:12 +0900

    initial commit

---
+++ MySearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySearchApp
+//
+//  Created by Sawa Takaaki on 2018/11/20.
+//  Copyright © 2018 Sawa Takaaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
